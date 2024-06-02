8ba668e01fdcd65067c522bd1db3cb9cebd9170e
Heriberto Amezcua <amezcua.cloud@gmail.com>
2024-06-02 16:12:20 +0200

    Hero section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,28 @@
 @extends('layouts.master')
 
-@section('title', 'Home')
+@section('title', 'Barista | Find the best coffee beans')
 
 @section('content')
+    <!-- SECTION HERO -->
+    <div class="section-hero">
+        <div class="hero">
+
+            <div class="hero__text-box">
+                <p class="heading-primary u-margin-bottom-small">
+                    Discover the Supreme Coffee Experience with Barista Excellence
+                </p>
+                <p class="text-normal u-margin-bottom-small">
+                    Don't let it slip away and enjoy every morning with the perfect aroma and flavor of our coffee, made
+                    with precision and passion by our state-of-the-art machine.
+                </p>
+                <a href="#" class="btn btn--cta">Shop Now</a>
+            </div>
+        </div>
+    </div>
+
+    
+
+
     <section class="section-home">
         <div class="container">
             <h2>Home</h2>
@@ -15,7 +35,7 @@
 
                     @foreach($products as $product)
 
-                            <x-product-card :product="$product"/>
+                        <x-product-card :product="$product"/>
 
                     @endforeach
 
